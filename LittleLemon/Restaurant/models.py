fe53991372cb49d95251b3082641c486199a080d
b0yx <khaled.s.alabsi@gmail.com>
2024-11-09 15:40:29 +0000

    Complete Database connection

--- LittleLemon/Restaurant/models.py
+++ LittleLemon/Restaurant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+# restaurant/models.py
+class Booking(models.Model):
+    name = models.CharField(max_length=100)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateField()
+    
+    def __str__(self):
+        return f"{self.name} - {self.booking_date}"
+    
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=3 , decimal_places=2)
+    inventory= models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.title} - {self.price}" 
